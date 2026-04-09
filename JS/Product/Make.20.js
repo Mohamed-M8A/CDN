e14b1c1092d5e0224058db39cf516d19740ca14d
Mohamed Adel <m7md20051968@gmail.com>
2026-04-10 01:46:26 +0200

    Update and rename Make.19.js to Make.20.js

--- JS/Product/Make.20.js
+++ JS/Product/Make.20.js
@@ -2,7 +2,7 @@
 
 (function() {
     const UILayout = {
-    injectEmptyShelf() {
+        injectEmptyShelf() {
             const root = document.getElementById('dynamic-shelf');
             if (!root || root.innerHTML.trim() !== "") return;
             root.innerHTML = `
@@ -22,26 +22,25 @@
                         <span class="price-saving"></span>
                     </div>
                     <span class="price-original"></span>
-                </div>
-                <div class="info-boxes-wrapper">
+                    </div>
+                    <div class="info-boxes-wrapper">
                     <div class="info-box product-variant"><span class="label">الموديل</span><span class="value variant-value">_</span></div>
                     <div class="info-box orders-count-box"><span class="label">الطلبات آخر 6 شهور</span><span class="value orders-count">_</span></div>
                     <div class="info-box product-availability"><span class="label">حالة المنتج</span><span class="value avail-value">_</span></div>
                     <div class="info-box country-shipping"><span class="label" id="shipLabel">الشحن</span><span class="value ship-to-value">_</span></div>
                     <div class="info-box shipping-time"><span class="label">مدة التوصيل</span><span class="value time-value">_</span></div>
                     <div class="info-box shipping-fee"><span class="label">رسوم التوصيل</span><span class="value fee-value">_</span></div>
-                </div>
+                    </div>
+                    <hr class="clean-divider">
 
-                <div class="button-container">
-                    <a href="#" class="buy-button" id="mainPurchaseBtn" target="_blank" rel="nofollow">-</a>
-                    <button class="add-to-cart" id="visitStoreBtn">-</button>
-                    <div id="telegram-alert-wrapper">-</div>
-                </div>
-
-                <hr class="clean-divider">
+                    <div class="button-container">
+                    <a href="#" class="buy-button" target="_blank" rel="nofollow">-</a>
+                    <button class="add-to-cart">-</button>
+                    <div id="telegram-alert-wrapper"></div>
+                    </div>
             `;
         },
-        
+
         drawStars(container, rating) {
             if (!container) return;
             const fullStars = Math.floor(rating);
